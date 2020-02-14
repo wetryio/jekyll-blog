679f3bc946fee8c56fe61b2ed006c14a435ab831
worming004 <worming@hotmail.be>
2020-02-14 09:02:36 +0100

    add test on localhost blog

---
+++ __tests__/homepage-test.js
@@ -0,0 +1,17 @@
+const request = require ('request');
+const parser = require ('node-html-parser');
+
+const localhosthttp = 'http://127.0.0.1';
+const bigVignetteSelectory = '#hero'
+
+test('Should render home page', () => {
+    request(localhosthttp, (error, response, body) => {
+        expect(error).toBeNull();
+
+        const root = parser.parse(body);
+
+        bigVignetteElem  = root.querySelector(bigVignetteSelectory);
+        expect(bigVignetteSelectory).not.toBeUndefined();
+        expect(bigVignetteSelectory).not.toBeNull();
+    });
+})
